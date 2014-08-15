20b9ebe714323065960ca7a7a72cafa07beda2dc
Seangel.Yu <Seangel_yuxh@hotmail.com>
2014-08-15 14:58:33 +0800

    Chapter.2. end

--- EloquentRuby/Chapter.2.Choose.Write.Control.Structure.rb
+++ EloquentRuby/Chapter.2.Choose.Write.Control.Structure.rb
@@ -1,7 +1,14 @@
 
-# if/unless  while/until  for->each
-# expression like normal speaking. 
-
+#---------------------------------------------------------
+# logical control
+# IMPORTANT NOTE: only false and nil are treated as false.
+# Do not intent to judge true, use false,
+# 
+#1. if/unless  while/until  for->each
+#2. expression like normal speaking. 
+#3. case when else(then) end: case use === to do comparison.
+#4. ?:  
+#5. lazy initialization with ||=
 
 
 class Document
@@ -56,11 +63,66 @@ class Document
     #   puts fonts
     # end
     # ->
-    fonts.each do |font|
-      puts font
-    end
+    #fonts.each do |font|
+    #  puts font
+    #end
+    # ->
+    fonts.each {|font| puts font}
   end
 
+
+  def demo_for_case
+    # use === to do comparison.
+    case title
+      when 'War and Peace'
+      puts 'Tolysoly'
+      when 'Romeo and Juliet'
+      puts 'Shakespare'
+      else
+      puts "Don't known"
+    end
+
+    # author = case title
+    #            when 'War and Peace'
+    #            'Tolysoly'
+    #            when 'Romeo and Juliet'
+    #            'Shakespare'
+    #            else
+    #            "Don't known"
+    #          end
+    # ->
+    
+    author = case title
+               when 'War and Peace' then 'Tolysoly'
+               when 'Romeo and Juliet' then 'Shakespare'
+               else "Don't known"
+               end
+
+    puts author
+    
+    # check class of an object 
+    case author
+    when Document then puts "It's a document instance"
+    when String then puts "It's a string instance"
+    else puts "Don't know what it is"
+    end
+               
+  end
+  
+  # use ?: if possible.
+  def demo_for_?condition
+    author = (title === 'Ware and Peace') ? 'Tolysoly': "Don't known"
+    puts author
+  end
+
+
+  # use ||= as possible as it can
+  def demo_for_lazy_initialize()
+    # @author  = '' unless @author
+    # ->
+    @author ||= ''
+  end
+    
 
 end
 
